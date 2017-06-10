0f52e2ee0d6f6830c4eab5b2a66fd67f89f2f4f2
Shun Hosaka <reyurnible@gmail.com>
2017-06-11 00:09:16 +0900

    [add] create project

---
+++ app/src/main/java/com/reyurnible/nrews/makeupkotlin/TopActivity.java
@@ -0,0 +1,13 @@
+package com.reyurnible.nrews.makeupkotlin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top);
+    }
+}
