be1a03bd6e19ccbd17ad5ace0ee9cf2284b420c1
Shun Hosaka <reyurnible@gmail.com>
2017-06-17 14:49:38 +0900

    add variable object

---
+++ app/src/main/kotlin/com/github/reyurnible/news/extension/Variable.kt
@@ -0,0 +1,42 @@
+package com.github.reyurnible.news.extension
+
+import io.reactivex.Observable
+import io.reactivex.subjects.BehaviorSubject
+
+/**
+ * Wrapping Rx BehaviorSubject defaultValue object.
+ */
+class Variable<T> private constructor(defaultValue: T?) {
+    companion object {
+        fun <T> create(): Variable<T> = Variable(null)
+        fun <T> createDefault(value: T): Variable<T> = createDefault(value)
+    }
+
+    // Call onNext is toSerialized()
+    val isAsyncEmit: Boolean = true
+    // Value holder
+    private val subject: BehaviorSubject<T>
+
+    init {
+        subject = defaultValue?.let {
+            BehaviorSubject.createDefault(it)
+        } ?: BehaviorSubject.create()
+    }
+
+    var value: T?
+        set(value) {
+            value?.let {
+                if (isAsyncEmit) subject.toSerialized().onNext(it)
+                else subject.onNext(it)
+            } ?: throw IllegalArgumentException("Value is not required null")
+        }
+        get() = if (subject.hasValue()) {
+            subject.value
+        } else {
+            null
+        }
+
+    fun value(defaultItem: T): T = subject.blockingFirst(defaultItem)
+
+    fun asObservable(): Observable<T> = subject.hide()
+}
