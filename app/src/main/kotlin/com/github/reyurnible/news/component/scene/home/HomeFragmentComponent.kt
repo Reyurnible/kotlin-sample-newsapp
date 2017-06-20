b4fee920d1a9a22583a2ba8c39643ba74b417980
Shun Hosaka <reyurnible@gmail.com>
2017-06-20 09:06:11 +0900

    fix home fragment component layout

--- app/src/main/kotlin/com/github/reyurnible/news/component/scene/home/HomeFragmentComponent.kt
+++ app/src/main/kotlin/com/github/reyurnible/news/component/scene/home/HomeFragmentComponent.kt
@@ -1,20 +1,28 @@
 package com.github.reyurnible.news.component.scene.home
 
-import android.support.v7.widget.LinearLayoutManager
 import android.view.View
-import org.jetbrains.anko.AnkoComponent
-import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.recyclerview.v7.recyclerView
+import com.github.reyurnible.news.R
+import org.jetbrains.anko.*
+import org.jetbrains.anko.design.tabLayout
 import org.jetbrains.anko.support.v4.viewPager
 
 /**
  * Home Scene Component
  */
 class HomeFragmentComponent : AnkoComponent<HomeFragment>, HomeView {
+    lateinit var pagerAdapter: HomePagerAdapter
 
     override fun createView(ui: AnkoContext<HomeFragment>): View = with(ui) {
-        viewPager {
+        pagerAdapter = HomePagerAdapter(ui.owner.childFragmentManager, emptyList())
+        verticalLayout {
+            val tabLayout = tabLayout {
 
+            }.lparams(matchParent, dimen(R.dimen.size))
+            viewPager {
+                adapter = pagerAdapter
+            }.lparams(matchParent, 0, weight = 1F).apply {
+                tabLayout.setupWithViewPager(this, true)
+            }
         }
     }
 
